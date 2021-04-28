685e0c1bc9bc3dfc19bfe1b48493ef76c29f8d65
its-dgreen <dylan@ctrlaltdylan.com>
2021-04-28 11:13:10 -0500

    feat: password reset workflow, more auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@
 Rails.application.routes.draw do
   root to: "main#index"
   
-  # GET /about
   get "about-us", to: "about#index", as: :about
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
@@ -12,4 +14,9 @@ Rails.application.routes.draw do
   post "sign_in", to: "sessions#create"
 
   delete "logout", to: "sessions#destroy"
+  
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 end
